951b888927bd32074475deb81114ca97c49a37ce
Nelson Andara <NelsonAndara@msn.com>
2019-02-22 12:07:00 -0600

    Update __init__.py

--- timeseries/__init__.py
+++ timeseries/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='timeseries-v0',
-    entry_point='timeseries.envs:timeseriesEnv',
+    entry_point='timeseries.envs:TimeseriesEnv',
 )
